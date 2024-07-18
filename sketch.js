8680512bb543dc0f9ee9480c0ca4cb80b244505f
AkilASC2024 <akilkamara5@gmail.com>
2024-07-18 13:49:35 -0400

    another commit

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let myXPos = 100;
+let myYPos = 100;
+
+function setup() {
+    createCanvas(500,500);
+    // noStroke();
+
+    rectMode(CENTER); // draw rectangles using coordinates as center
+}
+
+function draw() {
+    background(0);
+    fill(255,0,0);
+    rect(myXPos,myYPos,50);
+
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+        myXPos -= 3;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        myXPos += 3;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        myYPos -= 3;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        myYPos += 3;
+    }
+}
